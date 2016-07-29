ee9fae2a2e29e67495dacb19a1aeb06db707a7d0
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2016-07-29 11:10:56 +0300

    Remove deleted/unused packages from setup

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
     description='OCCI to Openstack/Cyclades API bridge',
     url='http://code.grnet.gr/projects/snf-occi',
     license='BSD',
-    packages=[
-        'snfOCCI', 'snfOCCI.snf_voms', 'snfOCCI.extensions', 'astavomaki'],
+    packages=['snfOCCI', 'snfOCCI.extensions'],
     entry_points='''
         [paste.app_factory]
         snf_occi_app = snfOCCI:main
